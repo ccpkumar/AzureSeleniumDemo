81e08a057d089be15b05b212b926dfa329259050
Prasanna <ccpkumar@yaho.com>
2023-03-31 16:06:24 +0530

    First commit

--- src/test/java/WM/DockerWithChrome.java
+++ src/test/java/WM/DockerWithChrome.java
@@ -19,7 +19,7 @@ public class DockerWithChrome {
 		URL url = new URL("http://localhost:4444/wd/hub");
 		RemoteWebDriver driver = new RemoteWebDriver(url, dc);
 		driver.get("https://opensource-demo.orangehrmlive.com/");
-		System.out.println("The tile is " + driver.getTitle());
+		System.out.println("The tile is  - " + driver.getTitle());
 		driver.quit();
 	}
 
